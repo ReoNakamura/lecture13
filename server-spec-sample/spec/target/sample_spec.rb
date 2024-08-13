e165f077b9a7b3687e2ec6b14a0852dea8349e83
中村玲央 <reonakamura@MacBook-Air.local>
2024-08-13 23:02:22 +0900

    うまくいかなかったので違うコマンドで再度テスト

--- server-spec-sample/spec/target/sample_spec.rb
+++ server-spec-sample/spec/target/sample_spec.rb
@@ -6,7 +6,7 @@ describe package('nginx') do
   it { should be_installed }
 end
 
-describe command('bash -lc "~/.rbenv/shims/ruby -v"') do
+describe command('source ~/.bash_profile && ~/.rbenv/shims/ruby -v's) do
   its(:stdout) { should match /ruby 3\.2\.3/ }
 end
 describe service('nginx') do
