87325230e4bceec9d625a6376f70ef4bbcddd37f
中村玲央 <reonakamura@MacBook-Air.local>
2024-08-13 22:40:22 +0900

    ssh経由で実行して環境変数が読み込まれてない可能性があるようなのでこの書き方で環境変数を読み込ませる

--- server-spec-sample/spec/target/sample_spec.rb
+++ server-spec-sample/spec/target/sample_spec.rb
@@ -6,7 +6,7 @@ describe package('nginx') do
   it { should be_installed }
 end
 
-describe command('~/.rbenv/shims/ruby -v') do
+describe command('bash -lc "~/.rbenv/shims/ruby -v"') do
   its(:stdout) { should match /ruby 3\.2\.3/ }
 end
 describe service('nginx') do
