967ff7b3761c6b610397e6c529fc4bc5a26fcc38
中村玲央 <reonakamura@MacBook-Air.local>
2024-08-13 23:23:04 +0900

    記述ミスがあったので修正

--- server-spec-sample/spec/target/sample_spec.rb
+++ server-spec-sample/spec/target/sample_spec.rb
@@ -6,7 +6,7 @@ describe package('nginx') do
   it { should be_installed }
 end
 
-describe command('source ~/.bash_profile && ~/.rbenv/shims/ruby -v's) do
+describe command('source ~/.bash_profile && ~/.rbenv/shims/ruby -v') do
   its(:stdout) { should match /ruby 3\.2\.3/ }
 end
 describe service('nginx') do
