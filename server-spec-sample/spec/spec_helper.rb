8eb1c33160914f98fdb8f8ab6fc1d7d815e94654
中村玲央 <reonakamura@MacBook-Air.local>
2024-08-13 09:46:22 +0900

    serverspecのリモートSSH接続してテストを実行してもらうように設定を変更しました、’circleci/config/yml’にもテスト実行に必要なcommandを追加しました

--- server-spec-sample/spec/spec_helper.rb
+++ server-spec-sample/spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require 'serverspec'
 
-set :backend, :exec
+set :backend, :ssh
 
+
+host = 'target'
+options = Net::SSH::Config.for(host)
+options[:user] = 'ec2-user'
+#options[:keys] = ''
+set :host, target
+set :ssh_options, options
